772c2e040557eac1db5c4b7afb42c8d30ab25c1d
Nguni Phakela <nguni52@gmail.com>
2023-05-18 21:36:05 +0200

    Add main module.

---
+++ src/app/pages/main/main.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainComponent } from './main.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [{
+  path: '',
+  component: MainComponent,
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MainRoutingModule { }
+
+@NgModule({
+  declarations: [
+    MainComponent
+  ],
+  imports: [
+    CommonModule,
+    MainRoutingModule
+  ]
+})
+export class MainModule { }
